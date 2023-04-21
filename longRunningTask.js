7b0b0208a10bf480c5cca3a60bc50d26048e62ee
joychester <joychester@gmail.com>
2023-04-20 17:24:30 -0700

    add async attr and move longRunningTask script external

---
+++ longRunningTask.js
@@ -0,0 +1,19 @@
+function startLongRunningTaskExecution() {
+    const start_time = performance.now();
+    // Simulate long-running execution with a loop and block load event
+    const iterations = 300000000; // Large number of iterations
+    //console.profile();
+    for (var i = 0; i < iterations; i++) {
+      // Simulated long-running code
+      // on Mac M1 Chip 300000000 iterations ~= 150ms
+    }
+    //console.profileEnd();
+
+    console.log(
+      `long running JS execution in function duration: ${
+        performance.now() - start_time
+      }`
+    );
+  }
+
+startLongRunningTaskExecution();
